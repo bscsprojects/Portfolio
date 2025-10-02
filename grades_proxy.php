faed5c16bc1cbb1f36c1f08af9f4f7d106b781d1
Projects <ascarez.jonavie@gmail.com>
2025-10-02 17:54:07 +0800

    Delete grades_proxy.php

--- grades_proxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Proxy to fetch grades XML (avoids CORS issues)
-header("Content-Type: application/xml");
-echo file_get_contents("https://class-grades-cs.mywebcommunity.org/grades_api.php");
-?>
